20d5cbbfaff77c617cf4c751df05f31e66558a99
Emiliano Menéndez <emystein@gmail.com>
2023-02-09 16:56:16 -0300

    Add screenshots for incomplete_purchase_order_lines

--- incomplete_purchase_orders/__manifest__.py
+++ incomplete_purchase_orders/__manifest__.py
@@ -2,6 +2,9 @@
     'name': 'Incomplete Purchase Orders',
     'category': 'Inventory/Inventory',
     'version': '1.0',
+    'description': """
+    Adds a filter for Purchase Order Lines 'Received quantity is less than requested'
+    """
     'depends': ['purchase', 'stock', 'basic_purchase_order_line_views'],
     'data': [
         'views/purchase_views.xml',
